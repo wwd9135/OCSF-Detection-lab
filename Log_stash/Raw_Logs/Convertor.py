c2718ff60575d054304765afa71db8df83493fdd
Will <wwd9135@gmail.com>
2026-08-27 21:08:23 +0100

    V1 _sysmon normalisation complete
    Sysmon V1 is done, this is a strong version to start with, now tuning and removing uneccessary fields is required.

--- Log_stash/Raw_Logs/Convertor.py
+++ Log_stash/Raw_Logs/Convertor.py
@@ -1,15 +1,15 @@
 # Goal- Take sysmonRaw and clean up file.
 # If text == Aug 25 19:09:56 siem01 logstash[37767]: remove completely and continue to next line
 new = []
-with open("sysmonRaw.txt", "r") as file:
+with open("Sysmon_raw_ver2.txt", "r") as file:
     lines = file.readlines()
 for i in lines:
-    i = i.replace("Aug 25 19:09:56 siem01 logstash[37767]:", "")
+    i = i.replace("Aug 27 20:54:54 siem01 logstash[2966]:", "")
     new.append(i)
 
 for i in new:
     print(i)
 
-with open("sysmonCleaned.txt", "w") as file:
+with open("sysmonCleaned2.txt", "w") as file:
     for i in new:
         file.write(i)
