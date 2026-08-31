4dcec83fe91ec04df47cfc73bbc98fa6eaede096
Will <wwd9135@gmail.com>
2026-08-31 17:11:09 +0100

    4624 initial yml draft done

--- Log_stash/Windows_event/Convertor.py
+++ Log_stash/Windows_event/Convertor.py
@@ -1,15 +1,15 @@
 # Goal- Take sysmonRaw and clean up file.
 # If text == Aug 25 19:09:56 siem01 logstash[37767]: remove completely and continue to next line
 new = []
-with open("FinalRaw.txt", "r") as file:
+with open("4624.txt", "r") as file:
     lines = file.readlines()
 for i in lines:
-    i = i.replace("Aug 31 13:00:03 siem01 logstash[5568]:", "")
+    i = i.replace("Aug 31 13:10:18 siem01 logstash[5568]:", "")
     new.append(i)
 
 for i in new:
     print(i)
 
-with open("FinalClean.txt", "w") as file:
+with open("4624clean.txt", "w") as file:
     for i in new:
         file.write(i)
