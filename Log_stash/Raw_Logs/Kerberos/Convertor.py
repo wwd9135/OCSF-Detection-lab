f05f2ae56f8cb0a8efe612d05fef3ed79b64ed59
Will <wwd9135@gmail.com>
2026-09-08 10:16:33 +0100

    4769 startpoint

--- Log_stash/Raw_Logs/Kerberos/Convertor.py
+++ Log_stash/Raw_Logs/Kerberos/Convertor.py
@@ -1,15 +1,15 @@
 # Goal- Take sysmonRaw and clean up file.
 # If text == Aug 25 19:09:56 siem01 logstash[37767]: remove completely and continue to next line
 new = []
-with open("Final.txt", "r") as file:
+with open("4769(raw1).txt", "r") as file:
     lines = file.readlines()
 for i in lines:
-    i = i.replace("Sep 06 15:22:09 siem01 logstash[30528]:", "")
+    i = i.replace("Sep 08 10:14:44 siem01 logstash[30528]:", "")
     new.append(i)
 
 for i in new:
     print(i)
 
-with open("Final_kerb.txt", "w") as file:
+with open("4769(1).txt", "w") as file:
     for i in new:
         file.write(i)
