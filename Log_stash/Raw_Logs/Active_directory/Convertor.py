6dd41fe74bc5c1f3778b5cce0549048715d6af62
Will <wwd9135@gmail.com>
2026-09-09 12:42:20 +0100

    51136 1st commit

--- Log_stash/Raw_Logs/Active_directory/Convertor.py
+++ Log_stash/Raw_Logs/Active_directory/Convertor.py
@@ -1,15 +1,15 @@
 # Goal- Take sysmonRaw and clean up file.
 # If text == Aug 25 19:09:56 siem01 logstash[37767]: remove completely and continue to next line
 new = []
-with open("4769final.txt", "r") as file:
+with open("1.txt", "r") as file:
     lines = file.readlines()
 for i in lines:
-    i = i.replace("Sep 08 14:29:28 siem01 logstash[33169]:", "")
+    i = i.replace("Sep 09 12:39:39 siem01 logstash[3549]:", "")
     new.append(i)
 
 for i in new:
     print(i)
 
-with open("4769(final).txt", "w") as file:
+with open("5136.txt", "w") as file:
     for i in new:
         file.write(i)
