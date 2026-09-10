c6648d47121712441cc61364cdc4db84d36e0507
Will <wwd9135@gmail.com>
2026-09-10 12:55:35 +0100

    4886/7 raw logs captured
    started working through 4886/7 cert authority telemetry.

--- Log_stash/Cert_authority/Convertor.py
+++ Log_stash/Cert_authority/Convertor.py
@@ -1,15 +1,15 @@
 # Goal- Take sysmonRaw and clean up file.
 # If text == Aug 25 19:09:56 siem01 logstash[37767]: remove completely and continue to next line
 new = []
-with open("1.txt", "r") as file:
+with open("combined1.txt", "r") as file:
     lines = file.readlines()
 for i in lines:
-    i = i.replace("Sep 09 12:39:39 siem01 logstash[3549]:", "")
+    i = i.replace("Sep 10 12:50:23 siem01 logstash[2856]:", "")
     new.append(i)
 
 for i in new:
     print(i)
 
-with open("5136.txt", "w") as file:
+with open("combined(1).txt", "w") as file:
     for i in new:
         file.write(i)
