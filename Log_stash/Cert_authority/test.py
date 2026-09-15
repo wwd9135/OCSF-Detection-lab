b0e820a36269f8521270f27c8f26d7d295bc9277
Will <wwd9135@gmail.com>
2026-09-15 11:13:35 +0100

    Update test.py

--- Log_stash/Cert_authority/test.py
+++ Log_stash/Cert_authority/test.py
@@ -1,7 +1,7 @@
 log = "event=4625 user=administrator src=10.0.0.8 status=failed"
 def parse_login(log):
     log = log.strip().lower()
-
+    for i in log
     key, valuew = log.split("=")
     print(valuew)
 parse_login(log)
